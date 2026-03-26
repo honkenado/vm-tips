7a8a368ef6e96b60c15ec8a7af40ea78697d6163
Henrik <honkenado@gmail.com>
2026-03-26 16:55:18 +0100

    add deadline lock

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
-export const DEADLINE = new Date('2026-06-10T23:00:00');
+export const DEADLINE = new Date('2020-06-10T23:00:00');
 export function isDeadlinePassed() {
   return new Date() > DEADLINE;
 }
