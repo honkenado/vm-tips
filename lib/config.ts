c4e4e396db7fc5b1c4c2287caa4ca2ee47c4f093
Henrik <honkenado@gmail.com>
2026-06-11 16:51:18 +0200

    deadlin upplåsning

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,3 @@
-export const DEADLINE = new Date('2026-06-10T23:59:00');
 export function isDeadlinePassed() {
-  return new Date() > DEADLINE;
+  return new Date() > new Date("2026-06-11T18:00:00+02:00");
 }
